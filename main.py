07d5a311975ed42120551c3b61b0d1ead4d8185b
枫子 <maple_leaf_7@msn.com>
2023-06-12 02:44:02 +0800

    first

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000), debug=False)
